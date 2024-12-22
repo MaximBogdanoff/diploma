e302f8475928a875286ff864bbc5f3dd511905b6
Максим <bogdanoff.96@yandex.ru>
2024-12-23 00:13:15 +1100

    Commit 1

--- src/main/java/ru/skypro/homework/service/AdService.java
+++ src/main/java/ru/skypro/homework/service/AdService.java
@@ -1,10 +1,14 @@
 package ru.skypro.homework.service;
 
 import ru.skypro.homework.dto.Ad;
+import ru.skypro.homework.dto.Comment;
 
 import java.util.List;
 
 public class AdService {
     public static List<Ad> getAdsByCurrentUser() {
+    }
+
+    public static List<Comment> getCommentsByAdId(Integer id) {
     }
 }
